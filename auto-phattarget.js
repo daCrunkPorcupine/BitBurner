1dc745b8cba9ee3d788bc83bedce00ae15ae11da
daCrunkPorcupine <juslod@gmail.com>
2022-01-12 06:57:39 -0500

    fixed error with numThreads

--- auto-phattarget.js
+++ auto-phattarget.js
@@ -17,7 +17,9 @@ export async function main(ns) {
         if (ns.getServerSecurityLevel(target) > securityCheck) {
             //Checks numThreads based on Free RAM * 80%
             numThreads = Math.floor((ns.getServerMaxRam("home") - ns.getServerUsedRam("home")) * 0.8) / ns.getScriptRam("auto-weaken.js");
-            ns.exec("auto-weaken.js", "home", numThreads, target);
+            if (numThreads >= 1) {
+                ns.exec("auto-weaken.js", "home", numThreads, target);
+            }
             await ns.sleep(1000);
             //ns.print("Checking if script is running:"+ns.isRunning("auto-weaken.js","home",server_target));
             while (ns.isRunning("auto-weaken.js", "home", target) == true) {
@@ -28,7 +30,9 @@ export async function main(ns) {
         else if (ns.getServerMoneyAvailable(target) < moneyCheck) {
             //Checks numThreads based on Free RAM * 80%
             numThreads = Math.floor((ns.getServerMaxRam("home") - ns.getServerUsedRam("home")) * 0.8) / ns.getScriptRam("auto-grow.js");
-            ns.exec("auto-grow.js", "home", numThreads, target);
+            if (numThreads >= 1) {
+                ns.exec("auto-grow.js", "home", numThreads, target);
+            }
             await ns.sleep(1000);
             //ns.print("Checking if script is running:"+ns.isRunning("auto-grow.js","home",server_target));
             while (ns.isRunning("auto-grow.js", "home", target) == true) {
