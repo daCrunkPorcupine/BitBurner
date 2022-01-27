07f714ec347f3c5a082590b2df381c9ef64e1741
daCrunkPorcupine <juslod@gmail.com>
2022-01-27 15:47:23 -0500

    split ns.purchaseTor() and ns.purchaseProgram();

--- api/singularity-tor.js
+++ api/singularity-tor.js
@@ -1,10 +1,5 @@
 /** @param {import(".").NS } ns */
 export async function main(ns) {
-    var exe = ns.args[0];
-    ns.tprint("/api/singularity-tor.js executing to " + exe);
     ns.purchaseTor();
     await ns.sleep(100);
-    ns.purchaseProgram(exe);
-    await ns.sleep(100);
-
 }
