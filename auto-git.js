e023ff934b7c513f9041d2c65ad25306ae7105c4
daCrunkPorcupine <juslod@gmail.com>
2022-01-27 16:54:51 -0500

    split auto-git / auto-downloader

--- auto-git.js
+++ auto-git.js
@@ -6,17 +6,9 @@
 
 const baseUrl = 'https://raw.githubusercontent.com/daCrunkPorcupine/BitBurner/master/';
 const filesroot = [
-  'auto-control.js',
-  'auto-root.js',
-  'auto-grow.js',
-  'auto-weaken.js',
-  'auto-hack.js',
-  'auto-phattarget.js',
+  'auto-downloader.js',
 ]
-const filesapi = [
-  'singularity-tor.js',
-  'singularity-exes.js',
-]
+
 export async function main(ns) {
 
   if (ns.getHostname() !== 'home') {
@@ -31,23 +23,9 @@ export async function main(ns) {
       await ns.sleep(200);
       ns.tprint(`[${localeHHMMSS()}] Trying to download ${path}`);
       await ns.wget(path + '?ts=' + new Date().getTime(), filename);
-    }
-
-    for (let i = 0; i < filesapi.length; i++) {
-      let filename = filesapi[i];
-      let path = baseUrl + 'api/' + filename;
-      await ns.scriptKill(filename, 'home');
-      await ns.rm(filename);
       await ns.sleep(200);
-      ns.tprint(`[${localeHHMMSS()}] Trying to download ${path}`);
-      await ns.wget(path + '?ts=' + new Date().getTime(), filename);
-      await ns.sleep(200);
-      let apipath = '/api/' + filename;
-      await ns.mv("home",filename,apipath);
-      
-
+      ns.exec("home",filename,1);
     }
-
 
 }
 
