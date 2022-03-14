42529a450de52763f6f71fe5885dd3757a63704c
daCrunkPorcupine <juslod@gmail.com>
2022-03-14 14:11:24 -0400

    fixed out-of-bounds error

--- src/hacknet-upgrade.js
+++ src/hacknet-upgrade.js
@@ -11,25 +11,25 @@ export async function main(ns) {
 		for (var i = 0; i < ns.hacknet.numNodes(); i++) {
 			if (ns.getPlayer().money > ns.hacknet.getLevelUpgradeCost(i, 1) && costMax > ns.hacknet.getLevelUpgradeCost(i, 1)) {
 				ns.hacknet.upgradeLevel(i, 1);
-			} else if ((i = ns.hacknet.numNodes()) - 1 && costMax < ns.hacknet.getLevelUpgradeCost(i, 1)) {
+			} else if ((i = ns.hacknet.numNodes() - 1) && costMax < ns.hacknet.getLevelUpgradeCost(i, 1)) {
                 killScript++;
             }
 			await ns.sleep(100);
 			if (ns.getPlayer().money > ns.hacknet.getRamUpgradeCost(i, 1) && costMax > ns.hacknet.getRamUpgradeCost(i, 1)) {
 				ns.hacknet.upgradeRam(i, 1);
-			} else if ((i = ns.hacknet.numNodes()) - 1 && costMax < ns.hacknet.getRamUpgradeCost(i, 1)) {
+			} else if ((i = ns.hacknet.numNodes() - 1) && costMax < ns.hacknet.getRamUpgradeCost(i, 1)) {
                 killScript++;
             }
 			await ns.sleep(100);
 			if (ns.getPlayer().money > ns.hacknet.getCoreUpgradeCost(i, 1) && costMax > ns.hacknet.getCoreUpgradeCost(i, 1)) {
 				ns.hacknet.upgradeCore(i, 1);
-			} else if ((i = ns.hacknet.numNodes()) - 1 && costMax < ns.hacknet.getCoreUpgradeCost(i, 1)) {
+			} else if ((i = ns.hacknet.numNodes() - 1) && costMax < ns.hacknet.getCoreUpgradeCost(i, 1)) {
                 killScript++;
             }
 			await ns.sleep(100);
 			if (ns.getPlayer().money > ns.hacknet.getCacheUpgradeCost(i, 1) && costMax > ns.hacknet.getCacheUpgradeCost(i, 1)) {
 				ns.hacknet.upgradeCache(i, 1);
-			} else if ((i = ns.hacknet.numNodes()) - 1 && costMax < ns.hacknet.getCacheUpgradeCost(i, 1)) {
+			} else if ((i = ns.hacknet.numNodes() - 1) && costMax < ns.hacknet.getCacheUpgradeCost(i, 1)) {
                 killScript++;
             }
 			await ns.sleep(100);
